5b4107e985f36b7b0f4bb10184d6f0bcb376fea2
Mohsen <mohsen-b@web.de>
2026-07-22 20:45:26 +0200

    Version 6

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 5 real Azure deploy!"
+    return "Version 6 real Azure deploy!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
